a661523e7adf79f8152a2d5452fe76514e9dc939
eneias <eoliveiradeliz@gmail.com>
2020-11-04 07:07:15 -0300

    Spring Boot Essential Training - amigoscode

---
+++ src/main/java/com/eneias/demo/dao/FakeStudentDaoImpl.java
@@ -0,0 +1,42 @@
+package com.eneias.demo.dao;
+
+import com.eneias.demo.model.Student;
+
+import java.util.*;
+
+public class FakeStudentDaoImpl implements StudentDao{
+
+    private final Map<UUID, Student> database;
+
+    public FakeStudentDaoImpl() {
+        database = new HashMap<>();
+    }
+
+    @Override
+    public int insertNewStudent(UUID studentId, Student student) {
+        database.put(studentId, student);
+        return 1;
+    }
+
+    @Override
+    public Student selectStudentById(UUID studentId) {
+        return database.get(studentId);
+    }
+
+    @Override
+    public List<Student> selectAllStudents() {
+        return new ArrayList<>(database.values());
+    }
+
+    @Override
+    public int updateStudentById(UUID studentId, Student studentUpdate) {
+        database.put(studentId, studentUpdate);
+        return 1;
+    }
+
+    @Override
+    public int deleteStudentById(UUID studentId) {
+        database.remove(studentId);
+        return 1;
+    }
+}
---
+++ src/main/java/com/eneias/demo/dao/StudentDao.java
@@ -0,0 +1,21 @@
+package com.eneias.demo.dao;
+
+import com.eneias.demo.model.Student;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface StudentDao {
+
+    int insertNewStudent(UUID studentId, Student student);
+
+    Student selectStudentById(UUID studentId);
+
+    List<Student> selectAllStudents();
+
+    int updateStudentById(UUID studentId, Student studentUpdate);
+
+    int deleteStudentById(UUID studentId);
+
+
+}
